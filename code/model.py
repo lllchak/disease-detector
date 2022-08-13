5a53c6ce6aa0aa8a9a676419ec679b92ed81b259
Pavel Lyulchak <mediumchak@yandex.ru>
2022-08-13 22:18:34 +0300

    weights_initializer

--- code/model.py
+++ code/model.py
@@ -4,44 +4,65 @@ import torch
 import torchvision
 
 
+vgg_pretrained = torchvision.models.vgg16(pretrained=True)
+
+
 class ConvNet(torch.nn.Module):
     def __init__(self, n_classes):
         super(ConvNet, self).__init__()
 
         self.features = torch.nn.Sequential(
             torch.nn.Conv2d(3, 64, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(64, 64, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, padding=2, return_indices=True),
 
             torch.nn.Conv2d(64, 128, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(128, 128, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, padding=2, return_indices=True),
 
             torch.nn.Conv2d(128, 256, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(256, 256, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(256, 256, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, padding=2, return_indices=True),
 
             torch.nn.Conv2d(256, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(512, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(512, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, padding=2, return_indices=True),
 
             torch.nn.Conv2d(512, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(512, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.Conv2d(512, 512, (3, 3), padding=1),
-            torch.nn.ReLu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, padding=2, return_indices=True)
         )
+
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(512 * 7 * 7, 4096),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.6),
+            torch.nn.Linear(4096, 4096),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.6),
+            torch.nn.Linear(4096, n_classes)
+        )
+
+        self.init_weights()
+
+    def init_weights(self):
+        for index, layer in enumerate(vgg_pretrained.features):
+            if isinstance(layer, torch.nn.Conv2d):
+                self.features[index].weight.data = layer.weight.data
+                self.features[index].bias.data = layer.bias.data
