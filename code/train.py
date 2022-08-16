1436954af9a481207e97a567a7952c33ca6f3eee
Pavel Lyulchak <mediumchak@yandex.ru>
2022-08-17 00:26:15 +0300

    fixed_train_module

---
+++ code/train.py
@@ -0,0 +1,39 @@
+import torch
+import model
+from tqdm.autonotebook import tqdm, trange
+
+
+def train(net: model.DeConvNet, epochs: int, use_gpu: bool, scheduler, optimizer, criterion, data) -> tuple:
+	net.train(True)
+
+	loss_history = []
+	pbar = trange(epochs, desc="Epoch")
+
+	for epoch in pbar:
+		scheduler.step()
+
+		curr_loss = 0.0
+
+		for batch in tqdm(data, leave=False, desc=f"{epoch} epoch"):
+			inputs, labels = batch
+			print(labels.shape)
+			if use_gpu:
+				inputs, labels = inputs.cuda(), labels.cuda()
+
+			optimizer.zero_grad()
+			outputs = net(inputs)
+
+			loss = criterion(outputs, labels)
+
+			loss.backward()
+			optimizer.step()
+
+			curr_loss += loss.item()
+
+		epoch_loss = curr_loss / 100
+
+		loss_history.append(epoch_loss)
+
+		pbar.set_description("Epoch: {} Loss: {:.4f}".format(epoch, epoch_loss))
+
+	return net, loss_history
