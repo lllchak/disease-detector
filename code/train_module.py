0174ac4cba380c12a63b22ad7a7e70a243be7e06
Pavel Lyulchak <mediumchak@yandex.ru>
2022-08-19 21:03:52 +0300

    added_scheduler

--- code/train_module.py
+++ code/train_module.py
@@ -1,21 +1,28 @@
 import torch
 import model
+import utils
 from tqdm.autonotebook import tqdm, trange
 
 
 def train(
     net: model.DeConvNet, epochs: int,
     use_gpu: bool, optimizer: torch.optim,
-    criterion: torch.nn.modules.loss, data: dict
+	scheduler: torch.optim.lr_scheduler,
+	criterion: torch.nn.modules.loss, data: dict
 ) -> tuple:
 
 	loss_history = {"train": [], "val": []}
+	accs_history = {"train": [], "val": []}
 	pbar = trange(epochs, desc="Epoch")
 
 	for epoch in pbar:
 
 		for phase in ["train", "val"]:
-			net.train(True) if phase == "train" else net.eval()
+			if phase == "train":
+				scheduler.step()
+				net.train(True)
+			else:
+				net.eval()
 			curr_loss = 0.0
 
 			for batch in tqdm(data[phase], leave=False, desc=f'{phase} iter'):
@@ -32,6 +39,7 @@ def train(
 					outputs = net(inputs)
 
 				loss = criterion(outputs, labels)
+
 
 				if phase == "train":
 					loss.backward()
