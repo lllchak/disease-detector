f1c5e6c10ee68c1ffbc45f78e11292e41562d52c
Pavel Lyulchak <mediumchak@yandex.ru>
2022-09-08 16:55:47 +0300

    added_accuracies_history

--- code/train_module.py
+++ code/train_module.py
@@ -24,6 +24,7 @@ def train(
 			else:
 				net.eval()
 			curr_loss = 0.0
+			curr_acc = 0.0
 
 			for batch in tqdm(data[phase], leave=False, desc=f'{phase} iter'):
 				inputs, labels = batch
@@ -39,16 +40,25 @@ def train(
 					outputs = net(inputs)
 
 				loss = criterion(outputs, labels)
+				iou_sum = 0.0
+				for i in range(len(outputs)):
+					pred = (outputs[i][0] > 0.65).to(dtype=int)
+					label = labels[i][0].to(dtype=int)
+
+					iou_sum += utils.iou_score(pred, label)
 
 				if phase == "train":
 					loss.backward()
 					optimizer.step()
 
 				curr_loss += loss.item()
+				curr_acc += iou_sum / len(outputs)
 
 			epoch_loss = curr_loss / 40 if phase == "train" else curr_loss / 5
+			epoch_acc = curr_acc / 40 if phase == "train" else curr_acc / 5
 
 			loss_history[phase].append(epoch_loss)
+			accs_history[phase].append(epoch_acc)
 
 			pbar.set_description("{} Loss: {:.4f}".format(phase, epoch_loss))
 
